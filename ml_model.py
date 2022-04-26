7280fde1e842b9b8d05e2124cd7d317ab92f93b6
SarikaSakore <sarika.sakore08@gmail.com>
2022-04-26 22:13:09 +0530

    added 3 new module code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding ML code")
+
+def module1():
+    print("adding code for ml module1")
+
+def module2():
+    print("adding code for ml module2")
+
+def module3():
+    print("adding code for ml module3")
